4195f8910fc910a2b40af7951cacfbe484f3f266
JatinJaglan347 <jatinjaglan347@gmail.comm>
2024-06-13 16:53:43 +0530

    update_Login/Signup

--- src/components/AuthUser/AuthUser.jsx
+++ src/components/AuthUser/AuthUser.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import image1 from '../../assets/3.jpg'
-import image2 from '../../assets/4.jpg'
+import image2 from '../../assets/7.jpg'
 
 function AuthUser() {
     const [windowSize, setWindowSize] = useState({
@@ -36,7 +36,7 @@ function AuthUser() {
     <>
         <div>
             <div className={`bg-[#131314] items-center text-center justify-center flex `} style={{height:` ${height}px`}}>
-                <div className={`md:w-[${formwidth}px] overflow-hidden shadow-lg  ${showLogin ? `shadow-[#646464]`: `shadow-[#02919B]` } rounded-2xl `} style={{ height:`${formheight}px`}}>
+                <div className={`md:w-[${formwidth}px] overflow-hidden shadow-lg  ${showLogin ? `shadow-[#ff7b7b]`: `shadow-[#02919B]` } rounded-2xl `} style={{ height:`${formheight}px`}}>
 
 {/* SIGNUP  */}
 
@@ -54,7 +54,7 @@ function AuthUser() {
                             <input className=' w-[100%] rounded-md px-3 py-1 backdrop-blur-sm bg-white/30 text-white' placeholder='Email' type="email" />
                             <input className=' w-[100%] rounded-md px-3 py-1 backdrop-blur-sm bg-white/30 text-white' placeholder='Password' type="password" />
                             <input className=' w-[100%] rounded-md px-3 py-1 backdrop-blur-sm bg-white/30 text-white' placeholder='Confirm Password' type="password" />
-                            <div className=' flex text-[#646464] gap-2'>
+                            <div className=' flex text-[#b1b1b1] gap-2'>
                                 <p>Already have an account? </p><a href="#" onClick={toggleForm}><h1 className='  text-[#a9a9a9] font-bold '> Login</h1></a>
                             </div>
                             <div>
@@ -65,7 +65,7 @@ function AuthUser() {
 
 {/* LOGIN  */}
 
-                    <div className={`transition-all duration-500 ease-in-out bg-cover bg-center bg-no-repeat items-center flex flex-col justify-center text-center rounded-b-2xl rounded-tl-[100px] md:rounded-tl-[130px] shadow-[0_-10px_30px_-15px_rgba(0,0,0,0.3)] shadow-[#646464] `} style={{
+                    <div className={`transition-all duration-500 ease-in-out bg-cover bg-center bg-no-repeat items-center flex flex-col justify-center text-center rounded-b-2xl rounded-tl-[100px] md:rounded-tl-[130px] shadow-[0_-10px_30px_-15px_rgba(0,0,0,0.3)] shadow-[#ff7b7b] `} style={{
                             backgroundImage: `url(${image2})`,
                             marginTop: showLogin ? `${-loginheight}px`: '-20%'
                         }}>
@@ -78,16 +78,16 @@ function AuthUser() {
                                         <input className=' w-[100%] rounded-md px-3 py-1 backdrop-blur-sm bg-white/30 text-white' placeholder='Email' type="email" />
                                         <input className=' w-[100%]  rounded-md px-3 py-1 backdrop-blur-sm bg-white/30 text-white' placeholder='Password' type="password" />
                                     </div>
-                                    <div className=' flex text-[#646464] gap-2 hover:text-[#bb905b]'>
+                                    <div className=' flex text-[#b1b1b1] gap-2 hover:text-[#ff7b7b]'>
                                         <a href=""><p>Forgot password? </p></a>
                                     </div>
                                 </div>
-                                <div className=' flex text-[#646464] gap-2'>
-                                    <p>Don't have an account? </p><a href="#" onClick={toggleForm}><h1 className=' text-[#a9a9a9] font-bold hover:text-[#8A7964]'>Signup</h1></a>
+                                <div className=' flex text-[#b1b1b1] gap-2'>
+                                    <p>Don't have an account? </p><a href="#" onClick={toggleForm}><h1 className=' text-[#a9a9a9] font-bold hover:text-[#ED0200]'>Signup</h1></a>
                                 </div>
 
                                 <div>
-                                    <button className=' w-full rounded-lg pt-2 pb-2 border inset-0 border-[#8A7964] text-white hover:text-[#8A7964] bg-[#171717]'>Submit</button>
+                                    <button className=' w-full rounded-lg pt-2 pb-2 border inset-0 border-[#ED0200] text-white hover:text-[#ED0200] bg-[#171717]'>Submit</button>
                                 </div>
                             </div>
                     </div>
