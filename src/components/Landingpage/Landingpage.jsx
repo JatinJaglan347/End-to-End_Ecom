2cc0b7f06b2ec8f4fb63f66d04024dbed17f4575
JatinJaglan347 <jatinjaglan347@gmail.comm>
2024-06-14 20:16:54 +0530

    Landing page responsive

--- src/components/Landingpage/Landingpage.jsx
+++ src/components/Landingpage/Landingpage.jsx
@@ -74,9 +74,9 @@ function Landingpage() {
     <>
         <div>
             <div className=' flex flex-col justify-center items-center text-center m-auto w-[80%] '>
-                <div className=' grid grid-cols-4 w-[100%]'>
-                    <div className='  col-span-3 '>
-                        <div className='  w-full h-full bg-white'></div>
+                <div className=' grid xl:grid-cols-4  w-[100%]'>
+                    <div className=' m-2 xl:col-span-3 md:'>
+                        <div className='rounded-2xl p-5    w-full h-full bg-black'></div>
                     </div>
 
 
@@ -84,7 +84,7 @@ function Landingpage() {
                         {filteredCategories1.map(category => (
                             <div className=' rounded-2xl p-5 m-2 bg-slate-100' key={category.id}>
                             <h2 className=' text-2xl pb-5 pl-3 font-bold'>{category.name}</h2>
-                            <div className=' grid grid-rows  '>
+                            <div className=' grid grid-rows-1 md:grid-cols-3 md:gap-5 xl:gap-0 xl:grid-cols-1'>
                             
                                 {category.subcategories.map(subcategory => (
                                 
@@ -103,11 +103,11 @@ function Landingpage() {
                     </div>
                 </div>
 
-                <div className=' grid grid-cols-4 grid-rows-2 w-[100%] '>
+                <div className=' grid lg:grid-cols-4 grid-rows-2 w-[100%] '>
                     {filteredCategories.map(category => (
                         <div className='  rounded-2xl p-5 m-2 col-span-2 bg-slate-100' key={category.id}>
                         <h2 className=' text-2xl pb-5 text-left pl-3 font-bold'>{category.name}</h2>
-                        <div className=' grid grid-cols-2 gap-5'>
+                        <div className=' grid md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-5'>
                             {category.subcategories.map(subcategory => (
                             <div className='  col-span-1 flex flex-col justify-center items-center' key={subcategory.id} >
                                 <Link to={`${subcategory.categoryname}`}>
@@ -121,11 +121,11 @@ function Landingpage() {
                     ))}
                 </div>
 
-                <div  className=' grid grid-cols-4  w-[100%] '>
+                <div  className=' grid lg:grid-cols-4  w-[100%] '>
                 {filteredCategories6.map(category => (
                     <div className=' rounded-2xl p-5 m-2 col-span-4 bg-slate-100' key={category.id}>
                     <h2 className=' text-2xl pb-5 pl-3 font-bold'>{category.name}</h2>
-                    <div className=' grid grid-cols-5 gap-8'>
+                    <div className=' grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-8'>
                         {category.subcategories.map(subcategory => (
                         <div className=' col-span-1 flex flex-col justify-center items-center' key={subcategory.id} > 
                             <Link to={`${subcategory.categoryname}`}>
